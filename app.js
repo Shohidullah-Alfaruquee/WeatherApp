c9c89b38622904dc96258b9cd9f4b843dd9087c8
shohidullah <shohidullah.ru@outlook.com>
2022-01-31 11:30:54 +0600

    local time, sunrise and sunset added

--- app.js
+++ app.js
@@ -13,31 +13,38 @@ let weather = {
         const {name} = data;
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
-        const { country, sunset } = data.sys;
+        const { country, sunset, sunrise } = data.sys;
         const { speed } = data.wind;
-        let date = new Date(sunset*1000);
-        let hours = date.getHours();
-        let minutes = "0" + date.getMinutes();
-        let seconds = "0" + date.getSeconds();
-        let sunsetTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+        let timer = ()=>{
+            let currentTime = new Date();
+            let currentLocalTime = currentTime.toLocaleTimeString();
+            document.getElementById("time").innerHTML = `Time: ${currentLocalTime}`;
+        }
+        timer()
+        setInterval(timer, 1000)
+
 
         document.getElementById("city").innerHTML = `Weather in ${name}, `;
-        document.getElementById("country").innerText = country;       
-        document.getElementById("temp").innerText = `${temp} °C`;
-        document.getElementById("sunset").innerText = `Sunset: ${sunsetTime}`
+        document.getElementById("country").innerHTML = country;       
+        document.getElementById("temp").innerHTML = `${temp} °C`;
+        let sunriseDate = new Date(sunrise*1000);
+        let sunrisetTime = sunriseDate.toLocaleTimeString()
+        document.getElementById("sunrise").innerHTML = `Sunrise: ${sunrisetTime}`
+        let date = new Date(sunset*1000);
+        let sunsetTime = date.toLocaleTimeString()
+        document.getElementById("sunset").innerHTML = `Sunset: ${sunsetTime}`
         document.getElementById("desc").innerText = description[0].toUpperCase() + description.substring(1);
         document.getElementById("humidity").innerText = `Humidity: ${humidity} %`;
         document.getElementById("wind").innerText = `Wind speed: ${speed} km/h`;
         document.getElementById('icon').src = "http://openweathermap.org/img/wn/" + icon + ".png";
 
     }
-}
+};
 
-
-;
 document.getElementById('searchBtn').addEventListener('click', ()=>{
     const cityName = document.getElementById('input-box').value;
     weather.fetchweather(cityName)
+    document.getElementById('input-box').value = ""
 })
 
 if(!document.getElementById('input-box').value){
@@ -47,5 +54,6 @@ if(!document.getElementById('input-box').value){
 document.getElementById('input-box').addEventListener('keyup', (event)=>{
     if(event.key == "Enter"){
         weather.fetchweather(document.getElementById('input-box').value);
+        document.getElementById('input-box').value = ""
     }
 })
